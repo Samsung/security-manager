tizenorg/platform.core.security.security-manager

Implementation of API for user management
Change-Id: Ib2cb08e1c466bc93775f8efe32dccd118ef095ad
Signed-off-by: Jan Cybulski <j.cybulski@samsung.com>

src/common/include/service_impl.h
@@ -81,6 +81,27 @@ int getPkgId(const std::string &appId, std::string &pkgId);
  */
 int getAppGroups(const std::string &appId, uid_t uid, pid_t pid, std::unordered_set<gid_t> &gids);
 
+/**
+ * Process user adding request.
+ *
+ * @param[in] uidadded uid of newly created user
+ * @param[in] user_type type of newly created user
+ * @param[in] uid uid of requesting user
+ *
+ * @return API return code, as defined in protocols.h
+ */
+int userAdd(uid_t uidadded, int user_type, uid_t uid);
+
+/**
+ * Process user deletion request.
+ *
+ * @param[in] uidadded uid of newly created user
+ * @param[in] uid uid of requesting user
+ *
+ * @return API return code, as defined in protocols.h
+ */
+int userDelete(uid_t uiddeleted, uid_t uid);
+
 } /* namespace ServiceImpl */
 } /* namespace SecurityManager */
 

src/server/service/service.cpp
@@ -141,6 +141,12 @@ bool Service::processOne(const ConnectionID &conn, MessageBuffer &buffer,
                 case SecurityModuleCall::APP_GET_GROUPS:
                     processGetAppGroups(buffer, send, uid, pid);
                     break;
+                case SecurityModuleCall::USER_ADD:
+                    processUserAdd(buffer, send, uid);
+                    break;
+                case SecurityModuleCall::USER_DELETE:
+                    processUserDelete(buffer, send, uid);
+                    break;
                 default:
                     LogError("Invalid call: " << call_type_int);
                     Throw(ServiceException::InvalidAction);
@@ -220,6 +226,30 @@ void Service::processGetAppGroups(MessageBuffer &buffer, MessageBuffer &send, ui
             Serialization::Serialize(send, gid);
         }
     }
+}
+
+void Service::processUserAdd(MessageBuffer &buffer, MessageBuffer &send, uid_t uid)
+{
+    int ret;
+    uid_t uidadded;
+    int user_type;
+
+    Deserialization::Deserialize(buffer, uidadded);
+    Deserialization::Deserialize(buffer, user_type);
+
+    ret = ServiceImpl::userAdd(uidadded, user_type, uid);
+    Serialization::Serialize(send, ret);
+}
+
+void Service::processUserDelete(MessageBuffer &buffer, MessageBuffer &send, uid_t uid)
+{
+    int ret;
+    uid_t uidremoved;
+
+    Deserialization::Deserialize(buffer, uidremoved);
+
+    ret = ServiceImpl::userDelete(uidremoved, uid);
+    Serialization::Serialize(send, ret);
 }
 
 

Samsung/security-manager

src/common/include/service_impl.h
@@ -81,6 +81,27 @@ int getPkgId(const std::string &appId, std::string &pkgId);
  */
 int getAppGroups(const std::string &appId, uid_t uid, pid_t pid, std::unordered_set<gid_t> &gids);
 
+/**
+ * Process user adding request.
+ *
+ * @param[in] uidAdded uid of newly created user
+ * @param[in] userType type of newly created user
+ * @param[in] uid uid of requesting user
+ *
+ * @return API return code, as defined in protocols.h
+ */
+int userAdd(uid_t uidAdded, int userType, uid_t uid);
+
+/**
+ * Process user deletion request.
+ *
+ * @param[in] uidDeleted uid of removed user
+ * @param[in] uid uid of requesting user
+ *
+ * @return API return code, as defined in protocols.h
+ */
+int userDelete(uid_t uidDeleted, uid_t uid);
+
 } /* namespace ServiceImpl */
 } /* namespace SecurityManager */
 

src/server/service/service.cpp
@@ -141,6 +141,12 @@ bool Service::processOne(const ConnectionID &conn, MessageBuffer &buffer,
                 case SecurityModuleCall::APP_GET_GROUPS:
                     processGetAppGroups(buffer, send, uid, pid);
                     break;
+                case SecurityModuleCall::USER_ADD:
+                    processUserAdd(buffer, send, uid);
+                    break;
+                case SecurityModuleCall::USER_DELETE:
+                    processUserDelete(buffer, send, uid);
+                    break;
                 default:
                     LogError("Invalid call: " << call_type_int);
                     Throw(ServiceException::InvalidAction);
@@ -220,6 +226,30 @@ void Service::processGetAppGroups(MessageBuffer &buffer, MessageBuffer &send, ui
             Serialization::Serialize(send, gid);
         }
     }
+}
+
+void Service::processUserAdd(MessageBuffer &buffer, MessageBuffer &send, uid_t uid)
+{
+    int ret;
+    uid_t uidAdded;
+    int userType;
+
+    Deserialization::Deserialize(buffer, uidAdded);
+    Deserialization::Deserialize(buffer, userType);
+
+    ret = ServiceImpl::userAdd(uidAdded, userType, uid);
+    Serialization::Serialize(send, ret);
+}
+
+void Service::processUserDelete(MessageBuffer &buffer, MessageBuffer &send, uid_t uid)
+{
+    int ret;
+    uid_t uidRemoved;
+
+    Deserialization::Deserialize(buffer, uidRemoved);
+
+    ret = ServiceImpl::userDelete(uidRemoved, uid);
+    Serialization::Serialize(send, ret);
 }
 
 