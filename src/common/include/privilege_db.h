tizenorg/platform.core.security.security-manager

Add security_manager_groups_for_uid()
This function returns resource-related groups for
given user.

Change-Id: I8b4a2bf2c2e85769543929e0ff5f0247dd60137a

src/common/include/privilege_db.h
@@ -129,7 +129,7 @@ private:
         { StmtType::EGetUserApps, "SELECT name FROM app WHERE uid=?" },
         { StmtType::EGetTizen2XPackages,  "SELECT DISTINCT pkg_name FROM app_pkg_view WHERE version LIKE '2.%%'" },
         { StmtType::EGetAppsInPkg, " SELECT app_name FROM app_pkg_view WHERE pkg_name = ?" },
-        { StmtType::EGetGroups, "SELECT DISTINCT group_name FROM privilege_group_view" },
+        { StmtType::EGetGroups, "SELECT DISTINCT group_name, privilege_name FROM privilege_group_view" },
         { StmtType::EGetPkgAuthorId, "SELECT author_id FROM pkg WHERE name = ? AND author_id IS NOT NULL"},
         { StmtType::EAuthorIdExists, "SELECT count(*) FROM author where author_id=?"},
         { StmtType::EGetAuthorIdByName, "SELECT author_id FROM author WHERE name=?"},
@@ -508,6 +508,16 @@ public:
      * @exception DB::SqlConnection::Exception::ConstraintError on constraint violation
      */
     void GetGroups(std::vector<std::string> &grp_names);
+
+    /**
+     * Retrieve vector of tuples with group_name (1st value) and privilege_name (2nd value)
+     *
+     * @param[out] privileges - list of privileges
+     * @exception DB::SqlConnection::Exception::InternalError on internal error
+     * @exception DB::SqlConnection::Exception::ConstraintError on constraint violation
+     */
+    void GetGroupsRelatedPrivileges(std::vector<std::tuple<std::string, std::string>> &privileges);
+
 };
 
 } //namespace SecurityManager

src/common/service_impl.cpp
@@ -1153,6 +1153,60 @@ int ServiceImpl::policyGetGroups(std::vector<std::string> &groups)
     return ret;
 }
 
+int ServiceImpl::policyGroupsForUid(uid_t uid, std::vector<std::string> &groups)
+{
+    int ret = SECURITY_MANAGER_SUCCESS;
+
+    try {
+        auto userType = CynaraAdmin::getInstance().GetUserType(uid);
+
+        if (userType == SM_USER_TYPE_NONE) {
+            return SECURITY_MANAGER_ERROR_NO_SUCH_OBJECT;
+        }
+
+        auto uidStr = std::to_string(uid);
+        int result;
+        std::string resultExtra;
+        std::string bucket;
+
+        switch (userType) {
+            case SM_USER_TYPE_NORMAL:
+                bucket = CynaraAdmin::Buckets.at(Bucket::USER_TYPE_NORMAL);
+                break;
+            case SM_USER_TYPE_ADMIN:
+                bucket = CynaraAdmin::Buckets.at(Bucket::USER_TYPE_ADMIN);
+                break;
+            case SM_USER_TYPE_GUEST:
+                bucket = CynaraAdmin::Buckets.at(Bucket::USER_TYPE_GUEST);
+                break;
+            case SM_USER_TYPE_SYSTEM:
+                bucket = CynaraAdmin::Buckets.at(Bucket::USER_TYPE_SYSTEM);
+                break;
+            default:
+                // Improperly configured
+                return SECURITY_MANAGER_ERROR_UNKNOWN;
+        }
+
+        std::vector<std::tuple<std::string, std::string>> group2privVector;
+        PrivilegeDb::getInstance().GetGroupsRelatedPrivileges(group2privVector);
+
+        for (const auto &g2p : group2privVector) {
+            CynaraAdmin::getInstance().Check(CYNARA_ADMIN_ANY, uidStr, std::get<1>(g2p),
+                                             bucket, result, resultExtra, true);
+            if (result == CYNARA_ADMIN_ALLOW)
+                groups.push_back(std::get<0>(g2p));
+        }
+    } catch (const CynaraException::Base &e) {
+        LogError("Error while getting user type from Cynara: " << e.DumpToString());
+        return SECURITY_MANAGER_ERROR_SERVER_ERROR;
+    } catch (const PrivilegeDb::Exception::Base &e) {
+        LogError("Error while getting groups from database: " << e.DumpToString());
+        return SECURITY_MANAGER_ERROR_SERVER_ERROR;
+    }
+
+    return ret;
+}
+
 int ServiceImpl::appHasPrivilege(
         std::string appName,
         std::string privilege,

src/include/app-runtime.h
@@ -102,6 +102,20 @@ int security_manager_prepare_app(const char *app_id);
 int security_manager_groups_get(char ***groups, size_t *groups_count);
 
 /**
+ * This function returns array of groups bound to privileges, the process
+ * run by particular user should get.
+ *
+ * Caller needs to free memory allocated for the list using
+ * security_manager_groups_free().
+ *
+ * @param[in] uid uid for user running the process
+ * @param[out] groups pointer to array of group names
+ * @param[out] groups_count number of strings in levels array
+ * @return API return code or error code.
+ */
+int security_manager_groups_get_for_uid(uid_t uid, char ***groups, size_t *groups_count);
+
+/**
  * This function frees memory allocated by security_manager_groups_get()
  * function.
  *

Samsung/security-manager

src/common/include/privilege_db.h
@@ -1,7 +1,7 @@
 /*
  * security-manager, database access
  *
- * Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2000 - 2016 Samsung Electronics Co., Ltd All Rights Reserved
  *
  * Contact: Rafal Krypa <r.krypa@samsung.com>
  *
@@ -31,6 +31,7 @@
 
 #include <cstdio>
 #include <list>
+#include <utility>
 #include <map>
 #include <stdbool.h>
 #include <string>
@@ -121,7 +122,7 @@ private:
         { StmtType::EGetUserApps, "SELECT app_name FROM user_app_pkg_view WHERE uid=?" },
         { StmtType::EGetTizen2XPackages,  "SELECT DISTINCT pkg_name FROM user_app_pkg_view WHERE version LIKE '2.%%'" },
         { StmtType::EGetAppsInPkg, " SELECT app_name FROM user_app_pkg_view WHERE pkg_name = ?" },
-        { StmtType::EGetGroups, "SELECT DISTINCT group_name FROM privilege_group" },
+        { StmtType::EGetGroups, "SELECT DISTINCT group_name, privilege_name FROM privilege_group" },
         { StmtType::EGetPkgAuthorId, "SELECT author_id FROM pkg WHERE name = ? AND author_id IS NOT NULL"},
         { StmtType::EAuthorIdExists, "SELECT count(*) FROM author where author_id=?"},
         { StmtType::EGetAuthorIdByName, "SELECT author_id FROM author WHERE name=?"},
@@ -453,6 +454,16 @@ public:
      * @exception DB::SqlConnection::Exception::ConstraintError on constraint violation
      */
     void GetGroups(std::vector<std::string> &grp_names);
+
+    /**
+     * Retrieve vector of pairs with group_name (1st value) and privilege_name (2nd value)
+     *
+     * @param[out] privileges - list of privileges
+     * @exception DB::SqlConnection::Exception::InternalError on internal error
+     * @exception DB::SqlConnection::Exception::ConstraintError on constraint violation
+     */
+    void GetGroupsRelatedPrivileges(std::vector<std::pair<std::string, std::string>> &privileges);
+
 };
 
 } //namespace SecurityManager

src/common/service_impl.cpp
@@ -1218,6 +1218,60 @@ int ServiceImpl::policyGetGroups(std::vector<std::string> &groups)
     return ret;
 }
 
+int ServiceImpl::policyGroupsForUid(uid_t uid, std::vector<std::string> &groups)
+{
+    int ret = SECURITY_MANAGER_SUCCESS;
+
+    try {
+        auto userType = CynaraAdmin::getInstance().GetUserType(uid);
+
+        if (userType == SM_USER_TYPE_NONE) {
+            return SECURITY_MANAGER_ERROR_NO_SUCH_OBJECT;
+        }
+
+        auto uidStr = std::to_string(uid);
+        int result;
+        std::string resultExtra;
+        std::string bucket;
+
+        switch (userType) {
+            case SM_USER_TYPE_NORMAL:
+                bucket = CynaraAdmin::Buckets.at(Bucket::USER_TYPE_NORMAL);
+                break;
+            case SM_USER_TYPE_ADMIN:
+                bucket = CynaraAdmin::Buckets.at(Bucket::USER_TYPE_ADMIN);
+                break;
+            case SM_USER_TYPE_GUEST:
+                bucket = CynaraAdmin::Buckets.at(Bucket::USER_TYPE_GUEST);
+                break;
+            case SM_USER_TYPE_SYSTEM:
+                bucket = CynaraAdmin::Buckets.at(Bucket::USER_TYPE_SYSTEM);
+                break;
+            default:
+                // Improperly configured
+                return SECURITY_MANAGER_ERROR_UNKNOWN;
+        }
+
+        std::vector<std::pair<std::string, std::string>> group2privVector;
+        PrivilegeDb::getInstance().GetGroupsRelatedPrivileges(group2privVector);
+
+        for (const auto &g2p : group2privVector) {
+            CynaraAdmin::getInstance().Check(CYNARA_ADMIN_ANY, uidStr, g2p.second,
+                                             bucket, result, resultExtra, true);
+            if (result == CYNARA_ADMIN_ALLOW)
+                groups.push_back(g2p.first);
+        }
+    } catch (const CynaraException::Base &e) {
+        LogError("Error while getting user type from Cynara: " << e.DumpToString());
+        return SECURITY_MANAGER_ERROR_SERVER_ERROR;
+    } catch (const PrivilegeDb::Exception::Base &e) {
+        LogError("Error while getting groups from database: " << e.DumpToString());
+        return SECURITY_MANAGER_ERROR_SERVER_ERROR;
+    }
+
+    return ret;
+}
+
 int ServiceImpl::appHasPrivilege(
         std::string appName,
         std::string privilege,

src/include/app-runtime.h
@@ -102,6 +102,20 @@ int security_manager_prepare_app(const char *app_id);
 int security_manager_groups_get(char ***groups, size_t *groups_count);
 
 /**
+ * This function returns array of groups bound to privileges, the process
+ * run by particular user should get.
+ *
+ * Caller needs to free memory allocated for the list using
+ * security_manager_groups_free().
+ *
+ * @param[in] uid uid for user running the process
+ * @param[out] groups pointer to array of group names
+ * @param[out] groups_count number of strings in levels array
+ * @return API return code or error code.
+ */
+int security_manager_groups_get_for_user(uid_t uid, char ***groups, size_t *groups_count);
+
+/**
  * This function frees memory allocated by security_manager_groups_get()
  * function.
  *